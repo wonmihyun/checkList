00adda621b1170ac4ab07da4c9107fe962bb684b
wonmihyun <mihyun6656@gmail.com>
2023-12-13 13:05:16 +0900

    "first-commit"

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          원미현
+          원미현 포트폴리오 체크리스트
         </p>
         <a
           className="App-link"
